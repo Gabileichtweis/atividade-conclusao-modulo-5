f5cff69c7bd6e2637ca96fb04ce040ad3ad8eca2
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-04-11 20:20:44 -0300

    Inicio página de cadastro

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,7 +1,72 @@
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
+import loginImage from '../../public/images/loginImage.png';
 
-const Cadastro: React.FC = () => {
-  return <h1>Cadastro</h1>;
+const Login: React.FC = () => {
+  return (
+    <>
+      <Grid container height={'100vh'}>
+        <Grid
+          item
+          height={'100%'}
+          xs={false}
+          sm={4}
+          md={8}
+          sx={{
+            backgroundImage: 'url()',
+          }}
+        />
+        <Grid item height={'100%'} xs={12} sm={8} md={4} bgcolor={'brown'}>
+          <Grid
+            container
+            height={'100%'}
+            alignItems={'center'}
+            bgcolor={'honeydew'}
+          >
+            <Grid
+              item
+              xs={12}
+              height={'50%'}
+              bgcolor={'gray'}
+              display={'flex'}
+              flexDirection={'column'}
+              justifyContent={'space-around'}
+              padding={3}
+            >
+              <Typography variant="h4" textAlign={'center'}>
+                Entrar
+              </Typography>
+              <Box component={'form'} textAlign={'center'}>
+                <TextField
+                  id="email"
+                  label={'E-mail'}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                />
+                <TextField
+                  id="password"
+                  label={'Senha'}
+                  variant="outlined"
+                  fullWidth
+                />
+                <TextField
+                  id="password2"
+                  label={'Confirmação de senha'}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                />
+              </Box>
+              <Button variant="contained" size="large">
+                Cadastrar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
-export default Cadastro;
+export default Login;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,59 +1,64 @@
 import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
+import loginImage from '../../public/images/loginImage.png';
 
 const Login: React.FC = () => {
   return (
-    <Grid container height={'100vh'} bgcolor={'blue'}>
-      <Grid
-        item
-        height={'100%'}
-        xs={false}
-        sm={4}
-        md={8}
-        bgcolor={'lightpink'}
-      ></Grid>
-      <Grid item height={'100%'} xs={12} sm={8} md={4} bgcolor={'brown'}>
+    <>
+      <Grid container height={'100vh'}>
         <Grid
-          container
+          item
           height={'100%'}
-          alignItems={'center'}
-          bgcolor={'honeydew'}
-        >
+          xs={false}
+          sm={4}
+          md={8}
+          sx={{
+            backgroundImage: 'url()',
+          }}
+        />
+        <Grid item height={'100%'} xs={12} sm={8} md={4} bgcolor={'brown'}>
           <Grid
-            item
-            xs={12}
-            height={'50%'}
-            bgcolor={'gray'}
-            display={'flex'}
-            flexDirection={'column'}
-            justifyContent={'space-around'}
-            padding={3}
+            container
+            height={'100%'}
+            alignItems={'center'}
+            bgcolor={'honeydew'}
           >
-            <Typography variant="h4" textAlign={'center'}>
-              Entrar
-            </Typography>
-            <Box component={'form'} textAlign={'center'}>
-              <TextField
-                id="email"
-                label={'E-mail'}
-                variant="outlined"
-                margin="normal"
-                fullWidth
-              />
-              <TextField
-                id="password"
-                label={'Senha'}
-                variant="outlined"
-                fullWidth
-              />
-            </Box>
-            <Button variant="contained" size="large">
-              Entrar
-            </Button>
+            <Grid
+              item
+              xs={12}
+              height={'50%'}
+              bgcolor={'gray'}
+              display={'flex'}
+              flexDirection={'column'}
+              justifyContent={'space-around'}
+              padding={3}
+            >
+              <Typography variant="h4" textAlign={'center'}>
+                Entrar
+              </Typography>
+              <Box component={'form'} textAlign={'center'}>
+                <TextField
+                  id="email"
+                  label={'E-mail'}
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                />
+                <TextField
+                  id="password"
+                  label={'Senha'}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Box>
+              <Button variant="contained" size="large">
+                Entrar
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
