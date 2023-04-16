1c93ecf77a68b888c679b99dac30f7196301e6c6
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-04-16 17:09:41 -0300

    sem nav bar

--- src/pages/Recados.tsx
+++ src/pages/Recados.tsx
@@ -1,21 +1,8 @@
 import { Grid } from '@mui/material';
 import React from 'react';
-import NavBar from '../componentes/NavBar';
 
 const Recados: React.FC = () => {
-  return (
-    <Grid
-      container
-      height={'100vh'}
-      justifyContent="center"
-      alignItems="flex-start"
-      bgcolor={'black'}
-    >
-      <Grid item height={'10%'} xs={12} sm={8} bgcolor={'coral'}>
-        <NavBar />
-      </Grid>
-    </Grid>
-  );
+  return <h1>Recados</h1>;
 };
 
 export default Recados;
